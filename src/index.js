48603b120f462a097a4e0b62c3631e4d7fbfbb97
Josh <JoshZirena@gmail.com>
2020-06-24 13:57:06 +0200

    feat: testing react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,91 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { } from 'react';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+let bookList = [
+  { title: "Hunger Games", author: "Suzzanne Collins", pages: 324, Illustrator: "Jamal Mashburn"},
+  { title: "The Sun Also Rises", author: "Ernest Hemingway", pages: 260},
+  { title: "Cat's Cradle", author: "Kurt Vongnut", pages: 304 },
+  { title: "White Teeth", author: "Zadie Smith", pages: 479},
+];
+
+const Book = ({title, author, pages, Illustrator}) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <p>by: {author}</p>
+      <p>pages: {pages} pages </p>
+      <p> Illustration by: {Illustrator}</p>
+    </section>
+  )
+}
+
+const Library = ({books}) => {
+  return (
+    <div>
+      {books.map((book, i) => 
+        <Book 
+          key = {i}
+          title = {book.title} 
+          author = {book.author}
+          Illustrator = {book.Illustrator}
+          pages = {book.pages}
+        />)}
+    </div>
+  )
+}
+
+render(
+  <Library books = {bookList}/>,
   document.getElementById('root')
-);
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
+// class SkiDayCounter extends Component {
+
+  // getPercent = decimal => {
+  //   return decimal * 100 + '%';
+  // }
+
+  // calcGoalProgress = (total, goal) => {
+  //   return this.getPercent(total/goal);
+  // }
+
+//   render() {
+//     const { total, powder, backCountry, goal } = this.props;
+//     return (
+//       <section>
+//       <div>
+//         <p> Total Days: {total} </p>
+//       </div>
+
+//       <div>
+//         <p> Powder Days: {powder} </p>
+//       </div>
+
+//       <div>
+//         <p> backCountry Days: {backCountry} </p>
+//       </div>
+
+//       <div>
+//         <p> Goal Progress: {this.calcGoalProgress(total, goal)} </p>
+//       </div>
+//     </section>
+//     )
+//   }
+// }
+
+// class Message extends React.Component {
+//   render() {
+//     console.log(this.props);
+//     return (
+//       <div>
+//         <h1 style = {{color: this.props.color}}>
+//           {this.props.msg}
+//         </h1>
+//         <p> I'll check back in {this.props.minutes} minutes </p>
+//       </div>
+//     )
+//   }
+// }
